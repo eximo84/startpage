18b111daebb3dfafbc1292657b829a938ef1f493
PenguinPaws <>
2020-07-18 14:38:27 -0400

    Add empty template pageData file

---
+++ js/pageData  - TEMPLATE.js
@@ -0,0 +1,109 @@
+
+var pageOptions = {
+    "openLinksInSameTab": true,
+    "searchEngineURL": "https://google.com/",
+    "displaySeconds": false,
+    "background": {
+        "randomize": true,
+        "minFileName": 20,
+        "maxFileName": 24,
+        "extension": "jpg",
+        "staticBackground": "backgrounds/999.jpg"
+    }
+}
+
+var columnData = [
+    {
+        "ContainerName": " ",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            }
+        ]
+    },
+    {
+        "ContainerName": " ",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            }
+        ]
+    },
+    {
+        "ContainerName": " ",
+        "SymbolPrefix": "fas fa-heart",
+        "ChildLinks": [
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            },
+            {
+                "DisplayName": "",
+                "Link": "",
+                "Image": ""
+            }
+        ]
+    }  
+];
